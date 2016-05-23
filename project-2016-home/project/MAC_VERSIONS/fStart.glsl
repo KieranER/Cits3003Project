1ee9a20b6c7140e98be490dbabe7d3e5336f7568
Jye Dewar <Jye-Dewar@users.noreply.github.com>
2016-05-23 17:21:44 +0800

    Update to Fstart to allow the second light to work

--- project-2016-home/project/MAC_VERSIONS/fStart.glsl
+++ project-2016-home/project/MAC_VERSIONS/fStart.glsl
@@ -1,11 +1,13 @@
 varying vec3 fN;
-varying vec3 fL;
+varying vec3 fL1;
+varying vec3 fL2;
 varying vec3 fE;
 varying vec2 texCoord;
 
 uniform vec3 AmbientProduct, DiffuseProduct, SpecularProduct;
 uniform mat4 ModelView;
-uniform vec4 LightPosition;
+uniform vec4 LightPosition1;
+uniform vec4 LightPosition2;
 uniform float Shininess;
 uniform sampler2D texture;
 
@@ -13,32 +15,58 @@ void main()
 {
     vec3 N = normalize(fN);
     vec3 E = normalize(fE);
-    vec3 L = normalize(fL);
+    vec3 L1 = normalize(fL1);
+    vec3 L2 = normalize(fL2);
     
-    vec3 H = normalize(L+E);
+    vec3 H1 = normalize(L1+E);
+    vec3 H2 = normalize(L2+E);
     vec4 ambient = vec4(AmbientProduct,1.0);
     
-    float Kd = max(dot(L, N), 0.0);
-    vec4 diffuse = Kd*vec4(DiffuseProduct,1.0);
+    float Kd1 = max(dot(L1, N), 0.0);
+    float Kd2 = max(dot(L2, N), 0.0);
+    vec4 diffuse1 = Kd1*vec4(DiffuseProduct,1.0);
+    vec4 diffuse2 = Kd2*vec4(DiffuseProduct,1.0);
     
-    float Ks = pow(max(dot(N, H), 0.0), Shininess);
-    vec4 specular = Ks*vec4(SpecularProduct,1.0);
+    float Ks1 = pow(max(dot(N, H1), 0.0), Shininess);
+    float Ks2 = pow(max(dot(N, H2), 0.0), Shininess);
+    vec4 specular1 = Ks1*vec4(SpecularProduct,1.0);
+    vec4 specular2 = Ks2*vec4(SpecularProduct,1.0);
     
-    if(dot(L,N) < 0.0){
-        specular = vec4(0.0,0.0,0.0,1.0);
+    if(dot(L1,N) < 0.0)
+    {
+        specular1 = vec4(0.0,0.0,0.0,1.0);
+    }
+    
+    if(dot(L2,N) < 0.0)
+    {
+        specular2 = vec4(0.0,0.0,0.0,1.0);
     }
     
     //adjust rgb values via a weighted average following luminosity standards for human vision, to create equivalent grayscale light for specular element.
-    float temp = 0.2989 * specular.x + 0.5870 * specular.y + 0.1140 * specular.z;
-    specular.xyz = vec3(temp,temp,temp);
+    float temp1 = 0.2989 * specular1.x + 0.5870 * specular1.y + 0.1140 * specular1.z;
+    specular1.xyz = vec3(temp1,temp1,temp1);
+    
+    float temp2 = 0.2989 * specular2.x + 0.5870 * specular2.y + 0.1140 * specular2.z;
+    specular2.xyz = vec3(temp2,temp2,temp2);
     
     // globalAmbient is independent of distance from the light source
-    vec4 globalAmbient = vec4(0.1, 0.1, 0.1,1.0);
+    vec4 globalAmbient = vec4(0.1, 0.1, 0.1, 1.0);
     
     //brightness fade by distance, doesn't affect global ambient
-    float atten = (0.3*length(fL)+0.2*length(fL)*length(fL));
+    //float atten = (0.3*length(fL1)+0.2*length(fL1)*length(fL1));
+    float atten = 0.5;
     
-    gl_FragColor = (globalAmbient + (ambient + diffuse + specular)/atten) * texture2D( texture, texCoord * 2.0 );
-    gl_FragColor.a = 1.0;
+    
+   
+  
+    
+    
+    //vec3 Light1 = diffuse1.xyz + specular1.xyz;
+    //vec3 Light2 = diffuse2.xyz + specular2.xyz;
+    vec4 color = (ambient + diffuse1 + specular1)/atten + globalAmbient;
+    color = color + (ambient + diffuse2 + specular2)/atten;
+    color.a = 1.0;
+    gl_FragColor = color * texture2D( texture, texCoord * 2.0 );
+
 }
 
