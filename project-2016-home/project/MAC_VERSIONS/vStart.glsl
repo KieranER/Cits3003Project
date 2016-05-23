057446c55a68889a9eb21188d1f1cada2b4b6ac2
Jye Dewar <Jye-Dewar@users.noreply.github.com>
2016-05-23 17:22:40 +0800

    Update to vstart to allow the second light to work

--- project-2016-home/project/MAC_VERSIONS/vStart.glsl
+++ project-2016-home/project/MAC_VERSIONS/vStart.glsl
@@ -4,11 +4,13 @@ attribute vec2 vTexCoord;
 
 varying vec3 fN;
 varying vec3 fE;
-varying vec3 fL;
+varying vec3 fL1;
+varying vec3 fL2;
 varying vec2 texCoord;
 
 uniform mat4 ModelView;
-uniform vec4 LightPosition;
+uniform vec4 LightPosition1;
+uniform vec4 LightPosition2;
 uniform mat4 Projection;
 
 void main()
@@ -16,13 +18,20 @@ void main()
     vec4 vpos = vec4(vPosition,1.0);
     
     vec3 pos = (ModelView * vpos).xyz;
+
+    vec4 Normal = vec4(vNormal,1.0);
     
-    fN = vNormal;
+    fN = (ModelView * Normal).xyz;
     fE = -pos;
-    fL = LightPosition.xyz;
+    fL1 = LightPosition1.xyz;
+    fL2 = LightPosition2.xyz;
     
-    if(LightPosition.w != 0.0){
-        fL = LightPosition.xyz - pos;
+    if(LightPosition1.w != 0.0){
+        fL1 = LightPosition1.xyz - pos;
+    }
+    
+    if(LightPosition2.w != 0.0){
+        fL2 = LightPosition2.xyz - pos;
     }
     
     gl_Position = Projection * ModelView * vec4(vPosition,1.0);
