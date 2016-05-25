ef8993a2fe26addbdb7e30781f10d6e322fbfc5e
KieranER <kerichards@westnet.com.au>
2016-05-25 23:13:18 +0800

    Updated vStart prepping for animations

--- project-2016-home/project/MAC_VERSIONS/vStart.glsl
+++ project-2016-home/project/MAC_VERSIONS/vStart.glsl
@@ -1,6 +1,8 @@
 attribute vec3 vPosition;
 attribute vec3 vNormal;
 attribute vec2 vTexCoord;
+attribute vec4 boneIDs;
+attribute vec4 boneWeights;
 
 varying vec3 fN;
 varying vec3 fE;
@@ -12,14 +14,20 @@ uniform mat4 ModelView;
 uniform vec4 LightPosition1;
 uniform vec4 LightPosition2;
 uniform mat4 Projection;
+uniform mat4 boneTransforms[64];
 
 void main()
 {
-    vec4 vpos = vec4(vPosition,1.0);
+    mat4 boneTransform = boneWeights[0]*boneTransforms[int(boneIDs[0])]+
+                        boneWeights[1]*boneTransforms[int(boneIDs[1])]+
+                        boneWeights[2]*boneTransforms[int(boneIDs[2])]+
+                        boneWeights[3]*boneTransforms[int(boneIDs[3])];
+    
+    vec4 vpos = boneTransform * vec4(vPosition,1.0);
     
     vec3 pos = (ModelView * vpos).xyz;
 
-    vec4 Normal = vec4(vNormal,1.0);
+    vec4 Normal = boneTransform * vec4(vNormal,1.0);
     
     fN = (ModelView * Normal).xyz;
     fE = -pos;
@@ -34,6 +42,6 @@ void main()
         fL2 = LightPosition2.xyz - pos;
     }
     
-    gl_Position = Projection * ModelView * vec4(vPosition,1.0);
+    gl_Position = Projection * ModelView * vpos;
     texCoord = vTexCoord;
 }
